42f39a5a3594148b521d1be3ab5fa59fa3fe1c8a
Krzysiek <krzysztofkasowicz1995@gmail.com>
2021-03-14 18:17:45 +0100

    added group middleware and 2 resources for route

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware('auth')->group(function(){
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    Route::resource('companies', \App\Http\Controllers\CompanyController::class);
+    Route::resource('notes', \App\Http\Controllers\NoteController::class);
+});
 
 require __DIR__.'/auth.php';

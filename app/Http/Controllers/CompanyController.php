a60b87d7f45abd283bb7c782146b0676933df3ab
Krzysiek <krzysztofkasowicz1995@gmail.com>
2021-03-14 18:18:51 +0100

    added crud methods to models

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    //
+    private $industries = ['automobile', 'clothing', 'food services', 'mining', 'transport'];
+    public function index()
+    {
+        $companies = Company::all();
+        return view('companies.index')->with('companies', $companies);
+    }
+
+    public function show($id)
+    {
+        $noteTypes = ['comment', 'rating', 'report'];
+        $notes = Note::all();
+        $company = Company::find($id);
+        return view('companies.show')->with('company', $company)->with('noteTypes', $noteTypes)->with('notes', $notes);
+    }
+
+    public function create(){
+        return view('companies.create')->with('industries', $this->industries);
+    }
+    public function store(Request $request)
+    {
+        $company = new Company;
+        $company->name = $request->input('name');
+        $company->NIP = $request->input('nip');
+        $company->zip_code = $request->input('zip_code');
+        $company->street = $request->input('street');
+        $company->city = $request->input('city');
+        $company->phone = $request->input('phone');
+        $company->email = $request->input('email');
+        $company->website = $request->input('website');
+        $company->industry = $request->input('industry');
+        $company->save();
+        return view('companies.create')->with('industries', $this->industries);
+    }
+    public function edit($id){
+        $company = Company::find($id);
+        return view('companies.edit')->with('company',$company)->with('industries', $this->industries);
+    }
+    public function update(Request $request, $id)
+    {
+        $company = Company::find($id);
+        $companies = Company::all();
+        $company->name = $request->input('name');
+        $company->NIP = $request->input('nip');
+        $company->zip_code = $request->input('zip_code');
+        $company->street = $request->input('street');
+        $company->city = $request->input('city');
+        $company->phone = $request->input('phone');
+        $company->email = $request->input('email');
+        $company->website = $request->input('website');
+        $company->industry = $request->input('industry');
+        $company->save();
+        return redirect('/companies');
+    }
 }
--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
 {
-    //
+    public function store(Request $request){
+
+        $note = new Note;
+        $note->message = $request->input('message');
+        $note->type = $request->input('type');
+        $note->company_id = $request->input('company_id');
+        $note->save();
+        return redirect('/companies');
+    }
 }
